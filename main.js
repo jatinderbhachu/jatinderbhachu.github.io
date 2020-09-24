fe53110dc306c2df898348e9203d064b58d62116
Jatinder Bhachu <jatinder.bhachu@gmail.com>
2020-09-23 23:16:32 -0400

    Initial Commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const navBar = document.querySelector("nav");
+const projectsSection = document.querySelector("#projects");
+
+//window.onscroll = () => {
+  //if(window.pageYOffset >= projectsSection.offsetTop){
+    //navBar.classList.add("navBackground");
+  //} else {
+    //navBar.classList.remove("navBackground");
+  //}
+//}
+const expKittensSlide = w3.slideshow('.expKittensScreenshots', 0);
+const graphingCalcSlide = w3.slideshow('.graphingCalcScreenshots', 0);
+const particleSimSlide = w3.slideshow('.particleSimScreenshots', 0);
+ 
+
